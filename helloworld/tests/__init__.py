dfe4404176ae22f740d5e823734b08645d38936c
Frank Jung <frankhjung@linux.com>
2025-03-01 16:17:56 +1100

    update pydocs

--- helloworld/tests/__init__.py
+++ helloworld/tests/__init__.py
@@ -1,1 +1,10 @@
-"""Unit tests for helloworld package."""
+"""
+Unit tests for helloworld package.
+
+Includes tests for:
+
+- helloworld.helloworld - simple hello world
+- helloworld.periods - reporting periods
+- helloworld.mylogging - setup logging
+
+"""
