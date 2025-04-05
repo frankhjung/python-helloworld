a6106b957a62424d5bf306d766d18c4454153cf5
Frank Jung <frankhjung@linux.com>
2025-04-05 15:41:03 +1100

    show reports and links

--- example/__init__.py
+++ example/__init__.py
@@ -1,1 +1,26 @@
-"""HelloWorld demo package."""
+"""
+Example Python Modules.
+
+This package contains example Python modules for
+- greeting the user
+- logging
+- calculating reporting periods
+- unit tests for all modules
+- command line interface
+- documentation
+
+## Reports and PyDoc
+
+- [Code Coverage (GitLab)](https://frankhjung1.gitlab.io/python-helloworld/coverage/index.html)
+- [Documentation (GitLab)](https://frankhjung1.gitlab.io/python-helloworld/example.html)
+- [Unit Tests (GitLab)](https://frankhjung1.gitlab.io/python-helloworld/pytest_report.html)
+
+## Resources
+
+- [bandit](https://bandit.readthedocs.io/en/latest/) for security
+- [pylint](https://pylint.pycqa.org/en/latest/) for code quality
+- [pytest](https://docs.pytest.org/en/latest/) for unit tests
+- [ruff](https://ruff.rs/) for code quality
+- [Sphinx](https://www.sphinx-doc.org/en/master/) for documentation
+
+"""
